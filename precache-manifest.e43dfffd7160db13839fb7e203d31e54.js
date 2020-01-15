1bd1ac968286d9df1fcd770167148fd5f3a9103f
Karol <56819265+lilivin@users.noreply.github.com>
2020-01-15 10:35:23 +0100

    Updates

---
+++ precache-manifest.e43dfffd7160db13839fb7e203d31e54.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8a99fd8cfbb348f591edc1c72b7d2d6",
+    "url": "/weatherapp/static/media/rainnight.a8a99fd8.jpg"
+  },
+  {
+    "revision": "248ffb6b8cf9f2ddb869",
+    "url": "/weatherapp/static/css/main.f4addbe1.chunk.css"
+  },
+  {
+    "revision": "cf924c0810c1488fda58",
+    "url": "/weatherapp/static/js/1.cf924c08.chunk.js"
+  },
+  {
+    "revision": "0e7ea501e7b8f22ce0b7",
+    "url": "/weatherapp/static/js/runtime~main.0e7ea501.js"
+  },
+  {
+    "revision": "56e4ce7d97686df11213e3decf4870da",
+    "url": "/weatherapp/static/media/rain.56e4ce7d.jpg"
+  },
+  {
+    "revision": "d9a1651b2b85594be220eeb2fdcddb8a",
+    "url": "/weatherapp/static/media/sun.d9a1651b.jpg"
+  },
+  {
+    "revision": "06db697c8f1eb8b0570ffb5abed71084",
+    "url": "/weatherapp/static/media/night.06db697c.jpg"
+  },
+  {
+    "revision": "248ffb6b8cf9f2ddb869",
+    "url": "/weatherapp/static/js/main.248ffb6b.chunk.js"
+  },
+  {
+    "revision": "77b86fce350ddfa9788330ea4b89168a",
+    "url": "/weatherapp/static/media/clouds.77b86fce.jpg"
+  },
+  {
+    "revision": "08a69299a62e5a2e8f89a96197c6732a",
+    "url": "/weatherapp/static/media/nightClouds.08a69299.jpg"
+  },
+  {
+    "revision": "9c5cdc561cd60042187e960415f257be",
+    "url": "/weatherapp/static/media/snow.9c5cdc56.jpg"
+  },
+  {
+    "revision": "9a6365b5f0fa8d8412207fd3b339ec4c",
+    "url": "/weatherapp/static/media/nightSnow.9a6365b5.jpg"
+  },
+  {
+    "revision": "ba4be2046df4e08cf4876d90d85eb096",
+    "url": "/weatherapp/static/media/background.ba4be204.png"
+  },
+  {
+    "revision": "702089ec3f5c634372e864f542093357",
+    "url": "/weatherapp/index.html"
+  }
+];
